895ff59f6d1c32bd8538a55b006e1bc7e7a3a5ce
Thanh Tai <thanhtai16969@gmail.com>
2024-02-04 05:50:19 +0700

    feat: view ar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import { Canvas } from "@react-three/fiber";
-import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+import {PresentationControls, Stage, useGLTF} from "@react-three/drei";
+import {useState} from "react";
 
 function Model(props) {
-  const { scene } = useGLTF("/bmw.glb");
+  const {scene} = useGLTF(props.path);
   return <primitive object={scene} {...props} />
 }
 
+const models = [{
+  key: "bmw",
+  image: "/bmw.png",
+  glpPath: "/bmw.glb"
+}, {
+  key: "dislo",
+  image: "/dislo.png",
+  glpPath: "/dislo.glb"
+}]
+
 function App() {
+  const [activeGlb, setActiveGlb] = useState("/bmw.glb")
   return (
-    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
-      <color attach="background" args={["#101010"]} />
-      <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
-        <Stage environment={"sunset"}>
-          <Model scale={0.01} />
-        </Stage>
-      </PresentationControls>
-    </Canvas>
+    <div style={{display: 'flex', gap: "0.5rem"}}>
+      <div style={{width: "15%"}}>
+        <div style={{display: 'flex', flexDirection: "column", gap: 20, cursor: 'pointer'}}>
+          {
+            models.map(model => (
+              <div key={model.key} onClick={() => setActiveGlb(model.glpPath)}>
+                <img alt={model.key} width={"100%"} src={model.image}/>
+                <span>{model.key}</span>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <div>
+        <GlbDom path={activeGlb}/>
+      </div>
+    </div>
   );
 }
 
+const GlbDom = ({path}) => {
+  return <Canvas dpr={[1, 2]} shadows camera={{fov: 45}} style={{"position": "absolute"}}>
+    <color attach="background" args={["#101010"]}/>
+    <PresentationControls speed={2.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+      <Stage environment={"sunset"}>
+        <Model scale={0.01} path={path}/>
+      </Stage>
+    </PresentationControls>
+  </Canvas>
+}
 export default App;
